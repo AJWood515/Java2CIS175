68a57af975d5a98b171c26e91bd174b0321f80be
AJWood515 <32825644+AJWood515@users.noreply.github.com>
2018-02-21 21:54:41 -0600

    SprindDemo-1st attempt FirstSpringProject-2nd attempt SpringProject-3rd attempt SpringAccount - following pdf Account Excerise

---
+++ FirstSpringProject/src/spring/Restaurant.java
@@ -0,0 +1,6 @@
+package spring;
+public class Restaurant {
+	public void greetCustomer() {
+		System.out.println("Welcome to our Resturaunt");
+	}
+}
---
+++ SpringProject/src/Spring/Restaurant.java
@@ -0,0 +1,6 @@
+package Spring;
+public class Restaurant {
+	public void greetCustomer() {
+		System.out.println("Welcome to our Resturaunt");
+	}
+}
